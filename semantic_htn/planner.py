d6bdeb3fbc44fe01aceab3f150d5a5b7dca8bafe
Alexandre Angleraud <alexandre.angleraud@gmail.com>
2020-04-23 21:52:32 +0300

    propagate target

--- semantic_htn/planner.py
+++ semantic_htn/planner.py
@@ -4,94 +4,6 @@ from cmd import Cmd
 from cobowl import world
 import copy
 
-class MyPrompt(Cmd):
-    def __init__(self):
-        super(MyPrompt, self).__init__()
-        self.world = DigitalWorld()
-        self.planner = Planner()
-        self.prompt = 'Panda> '
-        self.intro = """
-Collaborative Robotics Planning
--------------------------------
-
-Commands:
-     add (obj)
-     workspace_status
-     plan - to reason about the current status of the world
-     handover (obj) - to send the corresponding command and plan
-     pick (obj)
-     reach (obj)
-     grasp (obj)
-     take - to simulate a human taking something from the robot
-     assembly - to plan for the Cranfield Assembly benchmark
-     packaging - to try cleaning the workspace putting all the objects in a box
-     status - to show the robot properties
-     help
-"""
-
-    def do_exit(self, inp):
-        # Destroy the node explicitly
-        # (optional - otherwise it will be done automatically
-        # when the garbage collector destroys the node object)
-        self.planner.destroy_node()
-        return True
-
-    def do_plan(self, inp):
-        plan = self.planner.create_plan(self.world)
-        self.run(plan)
-
-    def do_add(self, inp):
-        self.world.add_object(inp)
-
-    def do_workspace_status(self, inp):
-        self.world.workspace.print_status()
-        if self.world.is_workspace_empty():
-            print("Workspace is empty")
-        else:
-            print("Workspace is not empty")
-
-    def do_take(self, inp):
-        self.world.send_command('release')
-        plan = self.planner.create_plan(self.world)
-        self.run(plan)
-
-    def do_pick(self, inp):
-        self.world.send_command('pick', inp)
-        plan = self.planner.create_plan(self.world)
-        self.run(plan)
-
-    def do_grasp(self, inp):
-        self.world.send_command('grasp', inp)
-        plan = self.planner.create_plan(self.world)
-        self.run(plan)
-
-    def do_reach(self, inp):
-        try:
-            print("Reaching {}".format(inp))
-            self.world.send_command('reach', inp)
-            plan = self.planner.create_plan(self.world)
-            self.run(plan)
-        except AnchoringError as e:
-            print("Could not understand {}".format(e.object))
-
-    def do_leads_to(self, inp):
-        self.world.leads_to(inp)
-
-    def do_handover(self, inp):
-        self.world.send_command('give', inp)
-        plan = self.planner.create_plan(self.world)
-        self.run(plan)
-
-    def do_packaging(self, inp):
-        self.world.send_command('clean')
-        plan = self.planner.create_plan(self.world)
-        self.run(plan)
-
-    def do_assembly(self, inp):
-        print("Coming soon")
-
-    def do_status(self, inp):
-        self.world.robot.print_status()
 
 class DispatchingError(Exception):
    def __init__(self, primitive):
@@ -110,25 +22,30 @@ class Planner():
         print("exploring effects({})".format(current_task))
         return True if self.planning_world.are_effects_satisfied(current_task) else False
 
-    def explore_compound_task(self, current_task):
+    def explore_compound_task(self, current_task, target):
         print("exploring compound task({})".format(current_task))
         method = self.planning_world.find_satisfied_method(current_task)
         print("chosen method {}".format(method))
+        print(target)
         if method:
             new_tasks = [task for task in self.planning_world.find_subtasks(method)]
+            if target:
+                for task in new_tasks:
+                    task.actsOn = target
             return new_tasks
 
     def search(self, final_plan, tasks_to_process):
-        print("Creating plan about")
-        print(tasks_to_process)
         if not tasks_to_process:
             return final_plan
         else:
             current_task = tasks_to_process.pop(0)
+            target = current_task.actsOn
             parent, type = self.planning_world.find_type(current_task)
             if type == "CompoundTask":
-                new_tasks = self.explore_compound_task(parent)
+                new_tasks = self.explore_compound_task(parent, target)
                 print("new_tasks {}".format(new_tasks))
+                for t in new_tasks:
+                    print(t.actsOn)
                 if len(new_tasks) == 1 and new_tasks[0].is_a[0].name == "State":
                     final_plan.insert(0, new_tasks[0])
                 elif new_tasks:
@@ -155,7 +72,6 @@ class Planner():
     def create_plan(self, current_world, root_task=None):
         try:
             final_plan = list()
-            print("create_plan {}".format(current_world))
             self.planning_world = current_world.clone()
             tasks_to_process = self.planning_world.root_task if not root_task else root_task
             final_plan = self.search(final_plan, tasks_to_process)
@@ -179,7 +95,6 @@ class Planner():
                 #while plan and not world.check_state(goal_state):
                 while plan:
                     primitive = plan.pop(0)()
-                    print("running..{}".format(primitive))
                     if primitive.is_a[0].name == "State":
                         pass
                         #goal_state = primitive
@@ -199,7 +114,7 @@ class Planner():
         try:
             world.apply_effects(primitive)
             print("RUN:{}".format(primitive))
-            time.sleep(2)
+            time.sleep(1)
             #self.sem_controller.interpret(primitive)
         except:
             raise DispatchingError(primitive)
